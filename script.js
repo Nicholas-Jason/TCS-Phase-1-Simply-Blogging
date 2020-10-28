187c9ce2c3afc9094a48f80ac7acd0782e276e5c
Nicholas-Jason Roache <njrr325@gmail.com>
2020-10-28 17:54:28 -0400

    added addArticle function

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function addArticle()
+{
+    var title = document.createElement("h2");
+    var content = document.createElement("p");
+    var d = document.createElement("div");
+    var atitle = document.getElementByName("Article_title").value;
+    var aarticle = document.getElementByName("Article").value;
+    if(atitle1!="")
+    {
+        alert(atitle);
+    }
+    
+    //title.innerHTML = atitle;
+    //content.append(aarticle);
+    //d.append(title);
+    //d.append(content);
+    //document.getElementById("blogs").append(d);
+    /*
+    if(document.getElementById("blogs").contains(atitle))
+    {
+        alert("It is there!");
+    }
 
+    else
+    {
+        alert("Not there");
+    }
+    */
+}
