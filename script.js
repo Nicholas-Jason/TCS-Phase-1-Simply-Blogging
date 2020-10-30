0f7b6590987b72faea9421494651a261ee193bde
Nicholas-Jason Roache <njrr325@gmail.com>
2020-10-30 17:32:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
+document.getElementById("btn-add").addEventListener("click",addArticle);
+var title = document.createElement("h2");
+var content = document.createElement("p");
+var s = document.createElement("section");
+var atitle = document.createTextNode(document.getElementByName("Article_title").value);
+var aarticle = document.createTextNode(document.getElementByName("Article").value);
 function addArticle()
 {
-    var title = document.createElement("h2");
-    var content = document.createElement("p");
-    var d = document.createElement("div");
-    var atitle = document.getElementByName("Article_title").value;
-    var aarticle = document.getElementByName("Article").value;
-    if(atitle1!="")
-    {
-        alert(atitle);
-    }
+    title.appendChild(atitle);
+    alert(atitle);
+    content.appendChild(aarticle);
+    s.appendChild(title);
+    s.appendChild(content);
+    document.getElementById("blogs").appendChild(s);
     
-    //title.innerHTML = atitle;
-    //content.append(aarticle);
-    //d.append(title);
-    //d.append(content);
-    //document.getElementById("blogs").append(d);
-    /*
     if(document.getElementById("blogs").contains(atitle))
     {
         alert("It is there!");
@@ -25,5 +22,5 @@ function addArticle()
     {
         alert("Not there");
     }
-    */
+    
 }
